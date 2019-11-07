793de2f9c453e5e787e133fe0820bcf1a0986057
Muhamad Rizqi Ramdani <55569266+MuhamadRizqiRamdani@users.noreply.github.com>
2019-11-07 21:41:36 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/pbo2/latihan59/detectiveconan/Test.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo2.latihan59.detectiveconan;
+import java.util.Scanner;
+/**
+ *
+ * @author asus
+ * NAMA     : Muhamad Rizqi Ramdani
+ * KELAS    : IF-2
+ * NIM      : 10118069
+ * Deskripsi Program : Detektif Conan
+ */
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Tokoh tokoh = new Tokoh();
+        
+        System.out.println("===Tokoh Dalam Detektif Konan===");
+        System.out.println("---Berdasarkan Keahliannya---");
+        tokoh.tampilDaftarKeahlian();
+        tokoh.setKeahlian(sc.nextInt());
+        
+        System.out.println("");
+        
+        tokoh.tampilKeahlian(tokoh.getKeahlian());
+    }
+}
---
+++ src/main/java/com/mycompany/pbo2/latihan59/detectiveconan/Tokoh.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo2.latihan59.detectiveconan;
+
+/**
+ *
+ * @author asus
+ * NAMA     : Muhamad Rizqi Ramdani
+ * KELAS    : IF-2
+ * NIM      : 10118069
+ * Deskripsi Program : Detektif Conan
+ */
+public class Tokoh extends Keahlian{
+    private int keahlian;
+    
+    public Tokoh() {
+        super();
+    }
+
+    public int getKeahlian() {
+        return keahlian;
+    }
+
+    public void setKeahlian(int keahlian) {
+        this.keahlian = keahlian;
+    }
+    
+    public void tampilKeahlian(int keahlian) {
+        System.out.println(getNamaKeahlian(keahlian) + " : ");
+        
+        if(keahlian <= 5) {
+            System.out.println(getKeahlian(keahlian));
+        } else {
+            super.tampilKeahlian();
+        }
+    }
+}
